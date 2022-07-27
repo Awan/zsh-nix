e2a877bea310fb25ed9ed44515d9a301f63d7eb8
Abdullah <abdullah@abdullah.today>
2022-07-28 00:48:18 +0500

    prompt setup
    adding some files to make my zsh prompt without some extra plugins.
    prompt will show battery status, git status etc...

---
+++ battery.zsh
@@ -0,0 +1,35 @@
+#!/usr/bin/env zsh
+# shellcheck disable=2034
+coded_by='
+
+In the name of Allah, the most Gracious, the most Merciful.
+
+ ▓▓▓▓▓▓▓▓▓▓
+░▓ Author ▓ Abdullah <https://abdullah.today>
+░▓▓▓▓▓▓▓▓▓▓ YouTube <https://YouTube.com/AbdullahToday>
+░░░░░░░░░░
+
+'
+
+function battery_charge {
+    b_now=$(< /sys/class/power_supply/BAT0/energy_now)
+    b_full=$(< /sys/class/power_supply/BAT0/energy_full)
+    b_status=$(< /sys/class/power_supply/BAT0/status)
+    charge=$(( (b_now * 10) / b_full ))
+
+    if (( charge > 5 )) || [[ "Charging" == $b_status ]]; then
+        psvar[5]=green
+    elif (( charge > 2 )) then
+        psvar[5]=yellow
+    else
+        psvar[5]=red
+    fi
+
+    psvar[6]=${(l:charge::▸:)}${(l:10-charge::▹:)}
+
+    if [[ "Charging" == $b_status ]]; then
+        psvar[7]=" +"
+    else
+        psvar[7]=
+    fi
+}
---
+++ init.zsh
@@ -0,0 +1,3 @@
+#!/usr/bin/env zsh
+
+for f in  ~/.config/zsh/plugins/ak-prompt/*(DN); do source $f; done
